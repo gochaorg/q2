3ae59388c88ddbb8b1d65f68397950ee6223bfcf
Kamnev Georgiy <nt.gocha@gmail.com>
2019-12-05 12:11:19 +0500

    fix

--- src/main/typescript/linq1.ts
+++ src/main/typescript/linq1.ts
@@ -10,26 +10,26 @@ import * as esParser from "esprima"
 const foo = new NamedRemoteDataSource<Foo>( 'http://localhost:19500/api', 'foo' )
 const bar = new NamedRemoteDataSource<Bar>( 'http://localhost:19500/api', 'bar' )
 // console.log( 'fetch()' )
-// foo.fetch( r=>console.log(r) )
+// foo.fetch( row=>console.log(row) )
 
 // console.log( 'where()' )
-// foo.where( r=>r.id==1 )
-//    .fetch( r=>console.log(r) )
+// foo.where( row=>row.id==1 )
+//    .fetch( row=>console.log(row) )
 
-// console.log('join()')
-// foo
-//    .where( foo=> foo.id==1 )
-//    .join( bar, (foo,bar)=> foo.id==bar.fooId )
-//    .fetch( r=>
-//       console.log( `${r.a.name} <- ${r.b.name}` )
-//    )
+console.log('== join() ==')
+foo
+   .where( foo=> foo.id==1 )
+   .join( bar, (foo,bar)=> foo.id==bar.fooId )
+   .fetch( row=>
+      console.log( `${row.a.name} <- ${row.b.name}` )
+   )
 
-console.log('select()')
-foo.select({
-   id: foo => foo.id,
-   id2: foo => foo.id+foo.id,
-   name: foo => foo.name,
-   name2: foo => foo.name+foo.name
-}).fetch( row=>{
-   console.log( 'id=', row.id, ' id2=',row.id2, ' name=',row.name, ' name2=',row.name2 )
-})
+// console.log('select()')
+// foo.select({
+//    id: foo => foo.id,
+//    id2: foo => foo.id+foo.id,
+//    name: foo => foo.name,
+//    name2: foo => foo.name+foo.name
+// }).fetch( row=>{
+//    console.log( 'id=', row.id, ' id2=',row.id2, ' name=',row.name, ' name2=',row.name2 )
+// })
